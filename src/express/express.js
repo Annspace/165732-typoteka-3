60f9f99902ccad643b65c9651203f7ef17b93d9b
Ann Kurenkova <anya.kurenkova1@yandex.ru>
2021-08-14 21:07:43 +0300

    module3-task2

--- src/express/express.js
+++ src/express/express.js
@@ -6,13 +6,42 @@ const articlesRouter = require(`./routes/articles`);
 const myRouter = require(`./routes/my`);
 
 const app = express();
-app.use(`/articles`, articlesRouter);
-app.use(`/my`, myRouter);
-app.get(`/`, (req, res) => res.send(`/`));
-app.get(`/register`, (req, res) => res.send(`/register`));
-app.get(`/login`, (req, res) => res.send(`/login`));
-app.get(`/search`, (req, res) => res.send(`/search`));
-app.get(`/categories`, (req, res) => res.send(`/categories`));
+
 app.listen(DEFAULT_PORT_EXPRESS, () => {
   console.log(`Сервер запущен на порту: ${DEFAULT_PORT_EXPRESS}`);
+});
+
+app.use(express.static(`${__dirname}/public`));
+
+app.set(`views`, `${__dirname}/templates`);
+app.set(`view engine`, `pug`);
+
+app.get(`/`, (req, res) => {
+  res.render(`main`);
+});
+
+app.get(`/login`, (req, res) => {
+  res.render(`login`);
+});
+
+app.get(`/register`, (req, res) => {
+  res.render(`sign-up`);
+});
+
+app.get(`/search`, (req, res) => {
+  res.render(`search-1`);
+});
+
+app.use(`/articles`, articlesRouter);
+
+app.use(`/my`, myRouter);
+
+app.get(`/categories`, (req, res) => res.render(`all-categories`));
+
+app.use((req, res) => {
+  res.status(404).render(`400`);
+});
+
+app.use((req, res) => {
+  res.status(500).render(`500`);
 });
--- src/express/routes/articles.js
+++ src/express/routes/articles.js
@@ -3,9 +3,8 @@
 const {Router} = require(`express`);
 const articlesRouter = new Router();
 
-articlesRouter.get(`/add`, (req, res) => res.send(`/articles/add`));
-articlesRouter.get(`/:id`, (req, res) => res.send(`/articles/:id ${req.params.id}`));
-articlesRouter.get(`/edit/:id`, (req, res) => res.send(`/articles/edit/:id ${req.params.id}`));
-articlesRouter.get(`/category/:id`, (req, res) => res.send(`/category/:id ${req.params.id}`));
+articlesRouter.get(`/add`, (req, res) => res.render(`new-post`));
+articlesRouter.get(`/:id`, (req, res) => res.render(`post`));
+articlesRouter.get(`/category/:id`, (req, res) => res.render(`articles-by-category`));
 
 module.exports = articlesRouter;
